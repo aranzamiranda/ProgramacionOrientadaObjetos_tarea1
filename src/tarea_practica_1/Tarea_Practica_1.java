1d003f07871d9c7158bb58f09039357a613dfb9b
chiky <chiky@LAPTOP-6K6VPD6N>
2019-08-20 22:10:03 -0500

    init commit

--- src/tarea_practica_1/Tarea_Practica_1.java
+++ src/tarea_practica_1/Tarea_Practica_1.java
@@ -15,144 +15,167 @@ public class Tarea_Practica_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //objeto de clase Perro
-        Perro primero = new Perro("Siberiano", "Grande", 10, "Negro", "Puppy");
-        primero.anos = 7;
-        primero.color = "Café";
-        primero.nombre = "Pelon";
-        primero.raza = "Husky";
-        primero.tamano = "Mediano";
-        //Objeto de clase Auto
-        Auto carro1 = new Auto("Nissan", "Negro", 1999, 273920, "Camioneta");
-        carro1.marca = "Ford";
-        carro1.color = "Azul";
-        carro1.precio = 2983782;
-        carro1.ano = 2009;
-        carro1.tipo = "limusina";
-        //Objeto de clase Arbol
-        Arbol arbol1 = new Arbol("Roble Mediterraneo", 78273, "Grande", 30, "Roble");
-        arbol1.nombre = "Eucalipto";
-        arbol1.numeroDeArboles = 4534;
-        arbol1.tamano = "Chico";
-        arbol1.anos = 10;
-        arbol1.familia = "Myrtaceae";
-        //Objeto de clase Pelicula
-        Pelicula peli1 = new Pelicula("La bella y la bestia", 2017, "Bill Condon", 120, 8, "Drama");
-        peli1.nombre = "Titanic";
-        peli1.ano = 1997;
-        peli1.director = "James Cameron";
-        peli1.minutos = 140;
-        peli1.calificacion = 8.9;
-        peli1.genero = "Drama y Romance";
-        //Objeto de clase Libro
-        Libro libro1 = new Libro("Juego de Tronos", 755, "George R.R. Martin", "Fantasia", "Plaza Janés");
-        libro1.nombre = "Hija de Humo y Hueso";
-        libro1.numPag = 400;
-        libro1.autor = "Laini Taylor";
-        libro1.genero = "Fantasia";
-        libro1.editorial = "Alfaguara";
-        //Objeto de clase Gato
-        Gato gato1 = new Gato("Anastasia", "Egipcio", 3, "Piel", "Mediano");
-        gato1.nombre = "Dani";
-        gato1.raza = "Siames";
-        gato1.anos = 4;
-        gato1.color = "Blanco con negro";
-        gato1.tamano = "Mediano";
-        //Objeto de clase InstrumentoMusical
-        InstrumentoMusical instrumento1 = new InstrumentoMusical("Violín", "Café caoba", 10, 564.3);
-        instrumento1.nombre = "Guitarra";
-        instrumento1.color = "Café claro";
-        instrumento1.numInstrumentos = 14;
-        instrumento1.precio = 345.6;
-        //Objeto de clase Cancion
-        Cancion sonido1 = new Cancion("Sabes", "Balada", 3.45, "Reik");
-        sonido1.nombre = "7 rings";
-        sonido1.genero = "Electronica";
-        sonido1.duracion = 3;
-        sonido1.autor = "Ariana Grande";
-        //Objeto de clase Computadora
-        Computadora compu1 = new Computadora("Hp", "Intel", 2, "Gris", 27.4);
-        compu1.marca = "Lenovo";
-        compu1.tipoProcesador = "AMD";
-        compu1.memoria = 3;
-        compu1.color = "Plateado";
-        compu1.tamPantalla = 34.6;
-        //Objeto de clase Camara
-        Camara cam1 = new Camara("Nikon", 20.5, "Negra", 5, "Nikkor AF-S DX");
-        cam1.marca = "Fuiji";
-        cam1.megapixeles = 25.3;
-        cam1.color = "Azul";
-        cam1.memoria = 0;
-        cam1.tipoLente = "X A5";
-        //Objeto de clase Mochila
-        Mochila moch1 = new Mochila("Totto", 670, "Rosa", 3.5, "maleta");
-        moch1.marca = "Jansport";
-        moch1.precio = 1100;
-        moch1.color = "Negra";
-        moch1.peso = 2.6;
-        moch1.tipo = "Morral";
-        //Objeto de clase Television
-        Television tele1 = new Television("Samsung", 98, "Smart", "Negra");
-        tele1.marca = "LG";
-        tele1.pulgadasPantalla = 32;
-        tele1.tipo = "Smart";
-        tele1.color = "Gris";
-        //Objeto de clase Conejo
-        Conejo conejito1 = new Conejo("Rabbit", "Belier Holandes", 2, 3.4, 0.34);
-        conejito1.nombre = "Milanesa";
-        conejito1.raza = "Silver Fox";
-        conejito1.edad = 1;
-        conejito1.peso = 4;
-        conejito1.estatura = 0.35;
-        //Objeto de clase Puerta
-        Puerta puert1 = new Puerta("Arcon", "Blanca", "Caoba", 2.0, 36);
-         puert1.marca = "Cortizo";
-         puert1.color = "Gris";
-         puert1.tipoMadera = "No madera";
-         puert1.altura = 2.0;
-         puert1.peso = 37;
-        //Objeto de clase Celular
-        Celular cel1 = new Celular("Iphone 8", "dual" , "Apple A12", 23000, 10, 405, "Plata");
-        cel1.marca = "Samsung";
-        cel1.camara = "3D";
-        cel1.procesador = "Exynos";
-        cel1.precio = 12000;
-        cel1.tamano = 9;
-        cel1.peso = 406;
-        cel1.color = "Blanco";
-        //Objeto de clase Libreta
-        Libreta lib1 = new Libreta("Norma", 56.6, 100, "Mezclilla", "Doble linea");
-                lib1.marca = "Scribe";
-                lib1.gramaje = 98.6;
-                lib1.paginas = 200;
-                lib1.color = "Tornasol";
-                lib1.tipo = "Cuadro chico";
-        //Objeto de clase Idioma
-        Idioma idioma1 = new Idioma("Español", "Mas de 20 paises", 27, 572000000);
-                idioma1.nombre = "Ingles";
-                idioma1.pais = "Mas de 50 paises";
-                idioma1.numSignos = 26;
-                idioma1.hablantes = 400000000;
-        //Objeto de clase Materia
-        Materias materia1 = new Materias("Biologia", "Seres vivos", 90, "Irma Guadalupe Sanchez");
-        materia1.nombre = "Historia";
-        materia1.campoDeConocimiento = "Historia de la humanidad";
-        materia1.calificacion = 70;
-        materia1.maestro = "Sandra Cruz";
-        //Objeto de clase Deporte
-        Deporte dep1 = new Deporte("Futbol", "Liga nacional mexicana", 11, "Marcos Cruz"); 
-        dep1.name = "Basquetbol";
-        dep1.liga = "LNBP";
-        dep1.jugadores = 40;
-        dep1.entrenador = "Antonio Ruiz";
-          //Objeto de clase Pescado
-        Pescado pesc1 = new Pescado("Pez globo", "Agua salada", 1, 3);
-        pesc1.nombre = "Tiburon blanco";
-        pesc1.tipo = "Agua salada";
-        pesc1.anos = 3;
-        pesc1.peso = 23;
-        
-        
+     Perro primero = new Perro("Siberiano", "Grande", 10, "Negro", "Puppy");
+     primero.setNombre("Rony");
+     System.out.println(primero.getNombre());
+     primero.setRaza("Pastor aleman");
+     System.out.println(primero.getRaza());
+     primero.setTamano("Grande");
+     System.out.println(primero.getTamano());
+     primero.setAnos(9);
+     System.out.println(primero.getAnos());
+     primero.setColor("Cafe");
+     System.out.println(primero.getColor());
+     Auto carro1 = new Auto("Nissan", "Negro", 1999, 273920, "Camioneta");
+     carro1.setMarca("Ford");
+     System.out.println(carro1.getMarca());
+     carro1.setColor("Azul");
+     System.out.println(carro1.getColor());
+     carro1.setAno(2000);
+     System.out.println(carro1.getAno());
+     carro1.setPrecio(978656);
+     System.out.println(carro1.getPrecio());
+     carro1.setTipo("Auto");
+     System.out.println(carro1.getTipo());
+     Arbol arbol1 = new Arbol("Roble Mediterraneo", 78273, "Grande", 30, "Roble");
+     arbol1.setNombre("Eucalipto");
+     System.out.println(arbol1.getNombre());
+     arbol1.setNumeroDeArboles(2983);
+     System.out.println(arbol1.getNumeroDeArboles());
+     arbol1.setTamano("Pequeño");
+     System.out.println(arbol1.getTamano());
+     arbol1.setAnos(50);
+     System.out.println(arbol1.getAnos());
+     arbol1.setFamilia("Myrtaceae");
+     System.out.println(arbol1.getFamilia());
+     Pelicula peli1 = new Pelicula("La bella y la bestia", 2017, "Bill Condon", 120, 8, "Drama");
+     peli1.setNombre("Titanic");
+     System.out.println(peli1.getNombre());
+     peli1.setGenero("Drama");
+     System.out.println(peli1.getGenero());
+     peli1.setAno(1997);
+     System.out.println(peli1.getAno());
+     peli1.setDirector("James Cameron");
+     System.out.println(peli1.getDirector());
+     peli1.setMinutos(139);
+     System.out.println(peli1.getMinutos());
+     peli1.setCalificacion(10);
+     System.out.println(peli1.getCalificacion());
+     
+     Libro libro1 = new Libro("Juego de Tronos", 755, "George R.R. Martin", "Fantasia", "Plaza Janés");
+     libro1.setNombre("Hija de humo y hueso");
+     System.out.println(libro1.getNombre());
+     libro1.setNumPag(450);
+     System.out.println(libro1.getNumPag());
+     libro1.setAutor("Laini Taylor");
+     System.out.println(libro1.getAutor());
+     libro1.setGenero("fantasia");
+     System.out.println(libro1.getGenero());
+     libro1.setEditorial("Alfaguara");
+     System.out.println(libro1.getEditorial());
+     Gato gato1 = new Gato("Anastasia", "Egipcio", 3, "Piel", "Mediano");
+     gato1.setNombre("tequila");
+     System.out.println(gato1.getNombre());
+     gato1.setRaza("minino");
+     System.out.println(gato1.getRaza());
+     gato1.setAnos(2);
+     System.out.println(gato1.getAnos());
+     gato1.setColor("cafe");
+     System.out.println(gato1.getColor());
+     gato1.setTamano("grande");
+     System.out.println(gato1.getTamano());
+     
+     InstrumentoMusical instrumento1 = new InstrumentoMusical("Violín", "Café caoba", 10, 564.3);
+     instrumento1.setNombre("guitarra");
+     System.out.println(instrumento1.getNombre());
+     instrumento1.setColor("cafe");
+     System.out.println(instrumento1.getColor());
+     instrumento1.setNumInstrumentos(8);
+     System.out.println(instrumento1.getNumInstrumentos());
+     instrumento1.setPrecio(799);
+     System.out.println(instrumento1.getPrecio());
+     
+     Cancion sonido1 = new Cancion("Sabes", "Balada", 3.45, "Reik");
+     sonido1.setNombre("7 rings");
+     System.out.println(sonido1.getNombre());
+     sonido1.setGenero("Pop");
+     System.out.println(sonido1.getGenero());
+     sonido1.setDuracion(3.45);
+     System.out.println(sonido1.getDuracion());
+     sonido1.setAutor("Ariana Grande");
+     System.out.println(sonido1.getAutor());
+     
+     Computadora compu1 = new Computadora("Hp", "Intel", 2, "Gris", 27.4);
+     compu1.setMarca("lenovo");
+     System.out.println(compu1.getMarca());
+     compu1.setTipoProcesador("AMD");
+     System.out.println(compu1.getTipoProcesador());
+     compu1.setMemoria(3);
+     System.out.println(compu1.getMemoria());
+     compu1.setColor("gris");
+     System.out.println(compu1.getColor());
+     compu1.setTamPantalla(28.3);
+     System.out.println(compu1.getTamPantalla());
+     
+     Camara cam1 = new Camara("Nikon", 20.5, "Negra", 5, "Nikkor AF-S DX");
+     cam1.setMarca("Nikon");
+     System.out.println(cam1.getMarca());
+     cam1.setColor("verde");
+     System.out.println(cam1.getColor());
+     cam1.setMegapixeles(30.6);
+     System.out.println(cam1.getMegapixeles());
+     cam1.setMemoria(6);
+     System.out.println(cam1.getMemoria());
+     cam1.setTipoLente("X A5");
+     System.out.println(cam1.getTipoLente());
+     Mochila moch1 = new Mochila("Totto", 670, "Rosa", 3.5, "maleta");
+     moch1.setMarca("Adidas");
+     System.out.println(moch1.getMarca());
+     moch1.setPrecio(679);
+     System.out.println(moch1.getPrecio());
+     moch1.setColor("Gris");
+     System.out.println(moch1.getColor());
+     moch1.setPeso(2.5);
+     System.out.println(moch1.getPeso());
+     moch1.setTipo("mariconera");
+     System.out.println(moch1.getTipo());
+     Television tele1 = new Television("Samsung", 98, "Smart", "Negra");
+     tele1.setMarca("LG");
+     System.out.println(tele1.getMarca());
+     tele1.setPulgadasPantalla(32);
+     System.out.println(tele1.getPulagadasPantalla());
+     tele1.setTipo("Smart");
+     System.out.println(tele1.getTipo());
+     tele1.setColor("Negra");
+     System.out.println(tele1.getColor());
+     Conejo conejito1 = new Conejo("Rabbit", "Belier Holandes", 2, 3.4, 0.34);
+     conejito1.setNombre("Rabito");
+     System.out.println(conejito1.getNombre());
+     conejito1.setRaza("Conejito");
+     System.out.println(conejito1.getRaza());
+     conejito1.setEdad(2);
+     System.out.println(conejito1.getEdad());
+     conejito1.setPeso(2.8);
+     System.out.println(conejito1.getPeso());
+     conejito1.setEstatura(0.45);
+     System.out.println(conejito1.getEstatura());
+     Puerta puert1 = new Puerta("Arcon", "Blanca", "Caoba", 2.0, 36);
+     
+     
+     Celular cel1 = new Celular("Iphone 8", "dual" , "Apple A12", 23000, 10, 405, "Plata");
+     
+     Libreta lib1 = new Libreta("Norma", 56.6, 100, "Mezclilla", "Doble linea");
+     
+     Idioma idioma1 = new Idioma("Español", "Mas de 20 paises", 27, 572000000);
+     
+     Materias materia1 = new Materias("Biologia", "Seres vivos", 90, "Irma Guadalupe Sanchez");
+     
+     Deporte dep1 = new Deporte("Futbol", "Liga nacional mexicana", 11, "Marcos Cruz");
+     
+     Pescado pesc1 = new Pescado("Pez globo", "Agua salada", 1, 3);
+     
+     
+     
         System.out.println();
     }
     
